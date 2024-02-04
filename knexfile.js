bd3a6ef9528112e790088b16ad2b35485f4bb704
Scott Knell <104751509+sc124578@users.noreply.github.com>
2024-02-04 04:52:44 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: "heffalump.db.elephantsql.com",
-      user: "slhgiuzg",
-      password: "ULb_uKAj-Rf0Kei7jQRL8yHKs__EBh3d",
-      database: "slhgiuzg",
+      host: 'heffalump.db.elephantsql.com',
+      user: 'slhgiuzg',
+      password: 'ULb_uKAj-Rf0Kei7jQRL8yHKs__EBh3d',
+      database: 'slhgiuzg',
       port: 5432,
     },
     pool: {
@@ -19,10 +19,10 @@ module.exports = {
   additionalDatabase: {
     client: "pg",
       connection: {
-        host: "heffalump.db.elephantsql.com",
-        user: "izoagnxs",
-        password: "aOKsevjpaXl4aEvmTlNJ1DY06hlFftMB",
-        database: "izoagnxs",
+        host: 'heffalump.db.elephantsql.com',
+        user: 'izoagnxs',
+        password: 'aOKsevjpaXl4aEvmTlNJ1DY06hlFftMB',
+        database: 'izoagnxs',
         port: 5432,
       },
       pool: {
