77eb22f2bf9bcad2463036bc79daaaddcdf04466
Scott Knell <104751509+sc124578@users.noreply.github.com>
2024-02-04 05:03:01 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = require('./App.js');  // Assuming App.js exports an instance of Express
 
 app.use(express.json());
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
